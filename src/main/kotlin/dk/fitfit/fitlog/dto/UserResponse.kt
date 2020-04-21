4ae7323de3dc8a57b309e7d77a00c3e460ed049e
sj <sebastianthegreatful@gmail.com>
2020-04-21 09:51:41 +0700

    Add name property to UserResponse

--- src/main/kotlin/dk/fitfit/fitlog/dto/UserResponse.kt
+++ src/main/kotlin/dk/fitfit/fitlog/dto/UserResponse.kt
@@ -3,6 +3,7 @@ package dk.fitfit.fitlog.dto
 import java.time.LocalDateTime
 
 data class UserResponse(
-        val created: LocalDateTime,
-        val id: Long
+    val name: String,
+    val created: LocalDateTime,
+    val id: Long
 )
